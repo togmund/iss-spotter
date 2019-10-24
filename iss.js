e3b57ca7bec6e55f9947d6412012e198cd25b620
Ted Ogmundson <togmund@gmail.com>
2019-10-24 23:10:15 +0000

    laying out the groundwork with requirements and connecting js files

---
+++ iss.js
@@ -0,0 +1,16 @@
+/*
+The Approach
+We'll be making API requests to three different services to solve this problem.
+
+Fetch our IP Address
+Fetch the geo coordinates (Latitude & Longitude) for our IP
+Fetch the next ISS flyovers for our geo coordinates
+Each later step is dependent on data from the previous one.
+
+We will eventually be able to use our app in the following way:
+
+> node index.js
+Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+*/
+
+module.exports = { issSomething };
